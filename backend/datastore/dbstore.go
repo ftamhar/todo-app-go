1e399fe8f41cc84a1f8a76099d347aa9b3629310
Rahmat Fathoni <noti.rahmat@gmail.com>
2021-12-11 14:47:15 +0800

    complete feature CRUD via DB Postgre

--- backend/datastore/dbstore.go
+++ backend/datastore/dbstore.go
@@ -4,8 +4,10 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/Xanvial/todo-app-go/model"
 	_ "github.com/lib/pq"
@@ -66,13 +68,93 @@ func (ds *DBStore) GetCompleted(w http.ResponseWriter, r *http.Request) {
 }
 
 func (ds *DBStore) GetIncomplete(w http.ResponseWriter, r *http.Request) {
+	var completed []model.TodoData
+
+	query := `
+		SELECT id, title, status
+		FROM todo
+		WHERE status = false
+	`
+
+	rows, err := ds.db.Query(query)
+	if err != nil {
+		log.Println("error on getting todo:", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var data model.TodoData
+		if err := rows.Scan(&data.ID, &data.Title, &data.Status); err != nil {
+			log.Println("error on getting todo:", err)
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		}
+
+		completed = append(completed, data)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(completed)
 }
 
 func (ds *DBStore) CreateTodo(w http.ResponseWriter, r *http.Request) {
+	title := r.FormValue("title")
+	query := `
+		INSERT INTO todo(title) values ($1) RETURNING id
+`
+	id := 0
+	ds.db.QueryRow(query, title).Scan(&id)
+	res := model.TodoData{
+		ID:     id,
+		Title:  title,
+		Status: false,
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(res)
 }
 
 func (ds *DBStore) UpdateTodo(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+	value := r.FormValue("status")
+
+	status, err := strconv.ParseBool(value)
+	if err != nil {
+		panic(err)
+	}
+	idx, err := strconv.Atoi(id)
+	if err != nil {
+		panic(err)
+	}
+	q := `
+		UPDATE todo SET status = $1 WHERE id = $2 RETURNING id, title, status
+`
+	var res model.TodoData
+	err = ds.db.QueryRow(q, status, idx).Scan(&res.ID, &res.Title, &res.Status)
+	if err != nil {
+		panic(err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(res)
 }
 
 func (ds *DBStore) DeleteTodo(w http.ResponseWriter, r *http.Request) {
+
+	id := mux.Vars(r)["id"]
+	idx, err := strconv.Atoi(id)
+	if err != nil {
+		panic(err)
+	}
+
+	q := `
+		DELETE FROM todo WHERE id = $1
+`
+	_, err = ds.db.Exec(q, idx)
+	if err != nil {
+		panic(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{
+		"status": "success",
+	})
 }
